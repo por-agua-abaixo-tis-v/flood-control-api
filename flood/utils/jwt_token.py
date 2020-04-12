78bf6479d129c1822b42fc2fdc8975f93eda7993
ViniciusBellagamba <viniciusbgpricardo@gmail.com>
2020-04-12 17:16:39 -0300

    Endpoint para validar autenticidade de token e seu payload

--- flood/utils/jwt_token.py
+++ flood/utils/jwt_token.py
@@ -3,6 +3,7 @@
 import jwt
 import datetime
 import os
+from flood.endpoints import endpoints_exception
 from flask import jsonify
 
 
@@ -11,5 +12,17 @@ def jwt_token(user_id, email, pswd):
     token = jwt.encode({'user_id': user_id, 'email': email, 'pswd': pswd, 'exp': exp_time}, os.getenv('TOKEN_SECRET'))
     return token.decode('UTF-8')
 
+
+def jwt_decode(token):
+    try:
+        payload = jwt.decode(token, os.getenv('TOKEN_SECRET'))
+    except jwt.exceptions.ExpiredSignatureError:
+        raise endpoints_exception(403, "TOKEN HAS EXPIRED")
+    except jwt.exceptions.InvalidSignatureError:
+        raise endpoints_exception(401, "UNAUTHORIZED")
+    except jwt.exceptions.InvalidTokenError:
+        raise endpoints_exception(403, "INVALID TOKEN")
+
+    return payload
 
 
